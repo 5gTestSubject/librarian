3524788304b84cbd772e04fcbd2cd309df1817a7
Jocelyn <jschrepp2121@gmail.com>
2026-01-20 18:33:26 -0500

    read & write

--- addons/librarian/ui/spreadsheet/cells/color_cell.gd
+++ addons/librarian/ui/spreadsheet/cells/color_cell.gd
@@ -15,3 +15,20 @@ func _ready() -> void:
     picker.color_modes_visible = false
     picker.sliders_visible = false
 
+func set_cell_neighbor_left(neighbor: NodePath) -> void:
+    focus_neighbor_left = neighbor
+
+func set_cell_neighbor_top(neighbor: NodePath) -> void:
+    focus_neighbor_top = neighbor
+
+func set_cell_neighbor_right(neighbor: NodePath) -> void:
+    focus_neighbor_right = neighbor
+
+func set_cell_neighbor_bottom(neighbor: NodePath) -> void:
+    focus_neighbor_bottom = neighbor
+
+func set_cell_next(next: NodePath) -> void:
+    focus_next = next
+
+func set_cell_previous(previous: NodePath) -> void:
+    focus_previous = previous
