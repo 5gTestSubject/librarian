9341b3891ccd3976b6af0014e9b786bb5bac8824
Jocelyn <jschrepp2121@gmail.com>
2026-01-26 19:19:50 -0500

    LibraryTagAccess

--- addons/librarian/scripts/library_info.gd
+++ addons/librarian/scripts/library_info.gd
@@ -1,8 +1,6 @@
 @tool
 extends Node
 
-const TagAccess := preload("res://addons/librarian/scripts/io/tag_access.gd")
-
 const AUTOLOAD_NAME := "LibraryInfo"
 const AUTOLOAD_NODE_PATH := ^"/root/LibraryInfo"
 
@@ -16,7 +14,7 @@ func load_all() -> void:
     return load_tags()
 
 func load_tags() -> void:
-    tags = TagAccess.load_tags()
+    tags = LibraryTagAccess.load_tags()
 
 func save_all() -> bool:
     return save_tags()
@@ -24,4 +22,4 @@ func save_all() -> bool:
 func save_tags() -> bool:
     var list = tags.values()
     list.sort_custom(func(left: LibraryTag, right: LibraryTag): return left.name < right.name)
-    return TagAccess.save_tags(list)
+    return LibraryTagAccess.save_tags(list)
